23e277b6713e622abfa53f188bcc5064bb3284cf
libuyi <1354804848@qq.com>
2016-08-12 18:29:47 +0800

    the first time to send file to github

---
+++ app/src/main/java/atguigu/com/day20160812_versioncontrol/MainActivity.java
@@ -0,0 +1,13 @@
+package atguigu.com.day20160812_versioncontrol;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+	}
+}
